fc98c9872a2f4ac8a33a1d84f25491366ea66aa2
Raul Gimenez murg <136567985+Raauul1996@users.noreply.github.com>
2023-06-26 11:47:08 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator() {
+  let sujeto = ["The dog", "My grandma", "His turtle", "My bird"];
+  let verbo = ["ate", "peed", "crushed", "broke"];
+  let CD = ["my homework", "the keys", "the car"];
+  let CCT = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomNumSujeto = Math.floor(Math.random() * sujeto.length);
+  let randomNumVerbo = Math.floor(Math.random() * verbo.length);
+  let randomNumCD = Math.floor(Math.random() * CD.length);
+  let randomNumCCT = Math.floor(Math.random() * CCT.length);
+  let excuse =
+    "<h1>" +
+    sujeto[randomNumSujeto] +
+    " " +
+    verbo[randomNumVerbo] +
+    " " +
+    CD[randomNumCD] +
+    " " +
+    CCT[randomNumCCT] +
+    "." +
+    "</h1>";
+  document.write(excuse);
+}
+excuseGenerator();
